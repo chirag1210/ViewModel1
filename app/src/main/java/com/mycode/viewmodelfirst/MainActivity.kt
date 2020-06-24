d75009f856577efe9c27bce85300557c8fcaf478
chirag1210 <chirag.chaudhari7@gmil.com>
2020-06-24 16:41:59 +0530

    space

--- app/src/main/java/com/mycode/viewmodelfirst/MainActivity.kt
+++ app/src/main/java/com/mycode/viewmodelfirst/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var count = 0
     lateinit var viewModel: MainActivityViewModel
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
